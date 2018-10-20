1003696212db527e49f0fdf8d6bee9d79712a7ec
arcanrun <arcan.run@gmail.com>
2018-10-21 00:40:52 +0300

    still making view

---
+++ main.py
@@ -0,0 +1,22 @@
+from view.CLIView import CLIView
+from model.MoodMovieModel import MoodMovieModel
+from model.facade.FacadeMoodMovie import FacadeMoodMovie
+from model.facade.subsystem.DBShelve import DBShelve
+from model.facade.subsystem.MovieSelectorTheMovieDborg import MovieSelectorTheMovieDborg
+
+
+"""
+The progarm is made for educational purposes (TDD, SOLID, DesignPatterns, MVC pattern)
+"""
+def app(view):
+    view.main_menu()
+    app(view)
+if __name__ == '__main__':
+    db = DBShelve()
+    movie_selector = MovieSelectorTheMovieDborg()
+    facade = FacadeMoodMovie(movie_selector, db)
+    model = MoodMovieModel(facade)
+    controller = None
+    view = CLIView(model, controller)
+
+    app(view)
--- view/CLIView.py
+++ view/CLIView.py
@@ -1,3 +1,4 @@
+import sys
 from .interfaces.IObserver import IObserver
 
 
@@ -8,3 +9,81 @@ class CLIView(IObserver):
 
     def update(self):
         pass
+
+    def main_menu(self):
+        print(
+"""
+1. Choose movie
+2. Bookmarks
+3. Exit
+"""
+        )
+        n = input()
+        dict = {
+            '1': self.mood_movie_menu,
+            '2': self.duty_menu,
+            '3': self.exit_app
+
+        }
+        dict.get(n, self.main_menu)()
+
+    def mood_movie_menu(self):
+        print("HELLO! WHAT EMOTIONS DO YOU WANT TO FEEL?")
+
+        print(
+"""
+1. Laugh 
+2. Sad
+3. Fear, horror
+4. Joy, fun
+5. Dive into the world of fantasy
+6. Family evening
+7. Evening with girlfriend \ boyfriend
+8. Excitement, drive, save the world, breathtaking
+9. Western 
+10.Tension
+11. Back to main menu
+	
+"""
+        )
+        n = input()
+        dict = {
+            '1': [35],  # comedy
+            '2': [18, 10752],  # drama, war
+            '3': [80, 27, 53, 9648],  # Crime Horror Thriller Mystery
+            '4': [16, 35],  # Animation Comedy
+            '5': [14, 878],  # Fantasy, Science Fiction
+            '6': [10751, 16],  # Family Animation[adult = FALSE]!!!!!
+            '7': [10749],  # Romance
+            '8': [28, 12],  # Action, Adventure
+            '9': [37],  # Western"
+            '10':[10752]  # War
+
+
+        }
+        choosed_movie = dict.get(n, self.main_menu)
+
+        movie = self.model.get_movie_from_api(choosed_movie)
+
+        border = '.'
+        upper_border = border * 40
+        footer_border = len(upper_border) * border
+        print(upper_border)
+        print('==TITLE==')
+        print(movie['title'])
+        print('==overview'.upper() + '==')
+        text =  movie['overview']
+        for i in range(0, len(text), len(upper_border)):
+            if i % len(upper_border) == 0:
+                text = text[:i] + '\n' + text[i:]
+        print(text)
+        print('==URL==')
+        print(movie['url_movie'])
+        print(footer_border)
+        self.mood_movie_menu()
+
+    def duty_menu(self):
+        self.main_menu()
+
+    def exit_app(self):
+        sys.exit('See Ya!')
